15b45877669dafbb90dd524fb3d8ee8a040b3407
mohit98thaker <mohuace@gmail.com>
2024-08-30 11:04:47 -0400

    hashCode and equals exp

--- src/main/java/org/mohit/hashAndEquals/Employee.java
+++ src/main/java/org/mohit/hashAndEquals/Employee.java
@@ -49,17 +49,46 @@ public class Employee {
 
 
     //We will generate equals method where empId, name is enough.
-
+    //Equals method is used to compare equality between two different objects.
+    //For eg, when storing in hashMap, there will be hashCode generated based on where to
+    //store the object in the bucket, once bucket is decided, the equals method will check
+    //where exactly inside the bucket should the object be stored.
     @Override
     public boolean equals(Object o) {
+        //If i always return true, that means every object is equal to every other object
+        //that means only 1 value will be stored in the hashMap, rest all will be treated
+        //as the object and would not be stored. However, remember this will only happen
+        //when two objects are stored in the same bucket. But if hashCode is generating
+        //different hashCodes for those objects, they will stored in different buckets so
+        //returning true won't matter because every object will be in their own separate
+        //bucket.
+        //return true;
+
+        //This would mean if two objects are logically same, they will still be considered different
+        //and stored as a separate element in the list of that bucket.
+        //note that if two objects are same, their hashCode (if implemented correctly), will be the same so
+        //they will be in the same bucket.
+        //return false;
+
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
         return empId == employee.empId && Objects.equals(name, employee.name);
     }
 
+    //Hashcode generates a unique id that will be stored inside a bucket of an array,
+    //two different objects can have the same hashCode
     @Override
     public int hashCode() {
+        //If i return a constant value, that means every object will be stored in the same bucket.
+        //This leads to performance issue, because when we try to lookup in a hashMap for a particular
+        //Employee obj (which is the key of a hashMap), it is no longer a O(1) operation because after
+        //getting into the bucket, all the elements of the hashMap are present in a list and then
+        //the equals method will be used to check which is our object that we are looking for and accordingly,
+        //that object is fetched. This will become an O(n) operation essentially. However, as long as the
+        //equals method is implemented correctly, the correctness of the code is not compromised but the performance
+        //is.
+        //return 2;
         return Objects.hash(empId, name);
     }
 
--- src/main/java/org/mohit/hashAndEquals/hashMain.java
+++ src/main/java/org/mohit/hashAndEquals/hashMain.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class hashMain {
     public static void main(String[] args) {
 
+        //A List of List or List of LinkedList is used behind the scenes of a hashMap.
         Map<Employee, Integer> mp = new HashMap();
 
         Employee em1 = new Employee(1, "mohit", "IT", 100000);
@@ -13,12 +14,14 @@ public class hashMain {
         Employee em3 = new Employee(3, "mike", "Security", 200000);
         Employee em4 = new Employee(4, "daniel", "secret agent", 1000000);
         Employee em5 = new Employee(5, "rachel", "Finance", 450000);
+        Employee em6 = new Employee(1, "mohit", "IT", 100000);
 
         mp.put(em1, em1.getEmpId());
         mp.put(em2, em2.getEmpId());
         mp.put(em3, em3.getEmpId());
         mp.put(em4, em4.getEmpId());
         mp.put(em5, em5.getEmpId());
+        mp.put(em6, em6.getEmpId());
 
         for (Employee employee : mp.keySet()) {
             System.out.println(employee + ":" + employee.getEmpId());
