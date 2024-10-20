0a2f512a83829e74fe698a878e05ac93801667bc
mohit98thaker <mohuace@gmail.com>
2024-10-20 12:50:47 -0400

    byte array output stream

--- src/main/java/org/mohit/filehandling/MyByteArrayOutputStream.java
+++ src/main/java/org/mohit/filehandling/MyByteArrayOutputStream.java
@@ -1,4 +1,37 @@
 package org.mohit.filehandling;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class MyByteArrayOutputStream {
+    public static void main(String[] args) {
+
+        try {
+            //Here the destination is array of bytes
+
+            String data = "this is the data need to be output";
+
+            ByteArrayOutputStream os  = new ByteArrayOutputStream(data.getBytes().length);
+
+            byte[] outputData = data.getBytes();
+
+            //This will take the byte array data and write to the output stream.
+            os.write(outputData);
+
+            //Accessing data from output stream
+            String osData = os.toString();
+
+            //OR accessing the byte array from the output stream
+            byte[] osByteArr = os.toByteArray();
+
+            System.out.println(osData);
+
+            os.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
